eef59cbd7a16ef9da7d4a27276732a672a5b7f56
Chen <131841805+ChenY6688@users.noreply.github.com>
2024-08-27 10:08:13 +0800

    create Pagination component

---
+++ src/components/Pagination.vue
@@ -0,0 +1,33 @@
+<template>
+  <VaPagination
+    :model-value="props.modelValue"
+    class="mb-3 justify-center sm:justify-start"
+    @update:model-value="updateCurrentPage"
+    :pages="pages"
+    input
+  />
+</template>
+
+<script lang="ts" setup>
+import { defineProps, defineEmits, computed } from 'vue';
+
+const props = defineProps<{
+  modelValue: number;
+  itemsPerPage: number;
+  totalCount: number;
+}>();
+
+const emit = defineEmits(['update:currentPage']);
+
+const pages = computed(() => {
+  return Math.ceil(props.totalCount / props.itemsPerPage);
+});
+
+const updateCurrentPage = (newPage: number) => {
+  emit('update:currentPage', newPage);
+};
+
+</script>
+
+<style scoped>
+</style>
